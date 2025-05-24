a4e977cc719cfbc80e4e890dd9f9e44754192a9e
mangwahyue <143898488+mangwahyue@users.noreply.github.com>
2025-05-24 19:11:30 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup
 
+setup(
+    name='utilitasparserkustom',  # Nama paket saat diinstal
+    version='0.1.0',
+    py_modules=['parser_utils'], # Menunjukkan file .py mana yang menjadi modul
+    install_requires=[
+        # Jika parser_utils.py Anda butuh library lain, tambahkan di sini
+        # 'regex', # Contoh, jika Anda pakai library regex pihak ketiga
+    ],
+    author='Nama Anda',
+    author_email='email@anda.com',
+    description='Kumpulan utilitas parser kustom.',
+    # url='https://github.com/usernameanda/utilitas-parser-saya', # Opsional
+)
